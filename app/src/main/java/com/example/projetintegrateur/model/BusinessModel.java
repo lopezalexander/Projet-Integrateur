12b17c1fb92a85f11f4af7bbb3db1b1589945d09
lopezalexander <lopez-alexander@hotmail.com>
2022-10-02 18:23:50 -0400

    added business model

---
+++ app/src/main/java/com/example/projetintegrateur/model/BusinessModel.java
@@ -0,0 +1,23 @@
+package com.example.projetintegrateur.model;
+
+public class BusinessModel {
+
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
