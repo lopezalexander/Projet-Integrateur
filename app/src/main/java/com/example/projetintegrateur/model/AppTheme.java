748ec3c74a22bc9d64293940c257e1f809add951
Adnan <elguennouni.adnan@gmail.com>
2022-10-12 19:17:40 -0400

    Design Boutons Profil et Parametres

--- app/src/main/java/com/example/projetintegrateur/model/AppTheme.java
+++ app/src/main/java/com/example/projetintegrateur/model/AppTheme.java
@@ -1,9 +1,12 @@
 package com.example.projetintegrateur.model;
 
+import com.example.projetintegrateur.R;
+
 public class AppTheme {
     private int backgroundColor = -16361597;
     private int searchBar_backgroundColor = -16361597;
     private String theme = "Midnight";
+    private int buttonBg = R.drawable.icon_container_settings2;
 
     //**************\\
     //  CONSTRUCTOR  \\
@@ -39,5 +42,13 @@ public class AppTheme {
 
     public void setTheme(String theme) {
         this.theme = theme;
+    }
+
+    public int getButtonBg() {
+        return buttonBg;
+    }
+
+    public void setButtonBg(int buttonBg) {
+        this.buttonBg = buttonBg;
     }
 }
